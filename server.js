0fd012b87baae3d5c9baf2fe59b4e0d071840407
ahvo <ashley.vo1210@gmail.com>
2024-03-13 19:45:35 -0600

    movie route

--- server.js
+++ server.js
@@ -86,27 +86,8 @@ router.post('/signin', function (req, res) {
     })
 });
 
+
 router.route('/movies')
-    .get((req, res) => {
-        console.log(req.body);
-        res = res.status(200);
-        if (req.get('Content-Type')) {
-            res = res.type(req.get('Content-Type'));
-        }
-        var o = getJSONObjectForMovieRequirement(req);
-        res.json(o);
-    }
-    )
-
-    .post((req, res) => {
-        var movieNew = new Movie();
-        movieNew.title = req.body.title;
-        movieNew.releaseDate = req.body.releaseDate;
-        movieNew.genre = req.body.genre;
-        movieNew.actorName = req.body.actorName;
-        movieNew.characterName = req.body.characterNameName;
-    })
-
     .delete((req, res) => {
         console.log(req.body);
         res = res.status(200);
